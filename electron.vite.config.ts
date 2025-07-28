cbe44a0b73059d7f3974cb68b3e92b8e15cb69c1
BradMoyetones <brad.moyetones@gmail.com>
2025-07-28 14:53:28 -0500

    chore: upgrade shadcn/ui and related deps

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,8 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   main: {
@@ -15,6 +17,6 @@ export default defineConfig({
         '@': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [react(), tailwindcss()]
   }
 })
